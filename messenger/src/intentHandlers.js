aff5ea64872ff39e4a99d60c888a1f592bfe3f90
Tonge <rashmt@amazon.com>
2015-06-27 00:48:56 -0700

    Added intent handlers

--- messenger/src/intentHandlers.js
+++ messenger/src/intentHandlers.js
@@ -4,191 +4,93 @@
 var textHelper = require('./textHelper'),
     storage = require('./storage');
 
+var session.userName = null;
+var session.repeatRecipient = false;
+var session.repeatMessage = false;
+
 var registerIntentHandlers = function (intentHandlers, skillContext) {
-    intentHandlers.NewGameIntent = function (intent, session, response) {
-        //reset scores for all existing players
-        storage.loadGame(session, function (currentGame) {
-            if (currentGame.data.players.length === 0) {
-                response.ask('New game started. Who\'s your first player?',
-                    'Please tell me who\'s your first player?');
-                return;
-            }
-            currentGame.data.players.forEach(function (player) {
-                currentGame.data.scores[player] = 0;
-            });
-            currentGame.save(function () {
-                var speechOutput = 'New game started with '
-                    + currentGame.data.players.length + ' existing player';
-                if (currentGame.data.players.length > 1) {
-                    speechOutput += 's';
-                }
-                speechOutput += '.';
-                if (skillContext.needMoreHelp) {
-                    speechOutput += '. You can give a player points, add another player, reset all players or exit. What would you like?';
-                    response.ask(speechOutput);
-                } else {
-                    response.tell(speechOutput);
-                }
-            });
-        });
-    };
-
-    intentHandlers.AddPlayerIntent = function (intent, session, response) {
-        //add a player to the current game,
-        //terminate or continue the conversation based on whether the intent
-        //is from a one shot command or not.
-        var newPlayerName = textHelper.getPlayerName(intent.slots.PlayerName.value);
-        if (!newPlayerName) {
-            response.ask('OK. Who do you want to add?');
-            return;
-        }
-        storage.loadGame(session, function (currentGame) {
-            var speechOutput,
-                reprompt;
-            if (currentGame.data.scores[newPlayerName] !== undefined) {
-                speechOutput = newPlayerName + ' has already joined the game.';
-                if (skillContext.needMoreHelp) {
-                    response.ask(speechOutput + ' What else?');
-                } else {
-                    response.tell(speechOutput);
-                }
-                return;
-            }
-            speechOutput = newPlayerName + ' has joined your game. ';
-            currentGame.data.players.push(newPlayerName);
-            currentGame.data.scores[newPlayerName] = 0;
-            if (skillContext.needMoreHelp) {
-                if (currentGame.data.players.length == 1) {
-                    speechOutput += 'You can say, I am Done Adding Players. Now who\'s your next player?';
-                    reprompt = textHelper.nextHelp;
-                } else {
-                    speechOutput += 'Who is your next player?';
-                    reprompt = textHelper.nextHelp;
-                }
-            }
-            currentGame.save(function () {
-                if (reprompt) {
-                    response.ask(speechOutput, reprompt);
-                } else {
-                    response.tell(speechOutput);
-                }
-            });
-        });
-    };
-
-    intentHandlers.AddScoreIntent = function (intent, session, response) {
-        //give a player points, ask additional question if slot values are missing.
-        var playerName = textHelper.getPlayerName(intent.slots.PlayerName.value),
-            score = intent.slots.ScoreNumber,
-            scoreValue;
-        if (!playerName) {
-            response.ask('sorry, I did not hear the player name, please say again?');
-            return;
-        }
-        scoreValue = parseInt(score.value);
-        if (isNaN(scoreValue)) {
-            console.log('Invalid score value = ' + score.value);
-            response.ask('sorry, I did not hear the points, please say again?');
-            return;
-        }
-        storage.loadGame(session, function (currentGame) {
-            var targetPlayer, speechOutput = '', newScore;
-            if (currentGame.data.players.length < 1) {
-                response.ask('sorry, no player has joined the game yet, what can I do for you?');
-                return;
-            }
-            for (var i = 0; i < currentGame.data.players.length; i++) {
-                if (currentGame.data.players[i] === playerName) {
-                    targetPlayer = currentGame.data.players[i];
-                    break;
-                }
-            }
-            if (!targetPlayer) {
-                response.ask('Sorry, ' + playerName + ' has not joined the game. What else?');
-                return;
-            }
-            newScore = currentGame.data.scores[targetPlayer] + scoreValue;
-            currentGame.data.scores[targetPlayer] = newScore;
-
-            speechOutput += scoreValue + ' for ' + targetPlayer + '. ';
-            if (currentGame.data.players.length == 1 || currentGame.data.players.length > 3) {
-                speechOutput += targetPlayer + ' has ' + newScore + ' in total.';
+    intentHandlers.MyNameIntent = function (intent, session, response) {
+        if (session.userName) {
+            response.ask("Am I not talking to " + session.userName + " ?");
+        } else {
+            userName = intent.slots.Member.value;
+            if (!userName) {
+                response.ask("Who am I talking to?", "Can you please state your name?");
             } else {
-                speechOutput += 'That\'s ';
-                currentGame.data.players.forEach(function (player, index) {
-                    if (index === currentGame.data.players.length - 1) {
-                        speechOutput += 'And ';
-                    }
-                    speechOutput += player + ', ' + currentGame.data.scores[player];
-                    speechOutput += ', ';
-                });
+                response.tell("Welcome " + userName);
             }
-            currentGame.save(function () {
-                response.tell(speechOutput);
-            });
-        });
-    };
+        }
+    }
 
-    intentHandlers.TellScoresIntent = function (intent, session, response) {
-        //tells the scores in the leaderboard and send the result in card.
-        storage.loadGame(session, function (currentGame) {
-            var sortedPlayerScores = [],
-                continueSession,
-                speechOutput = '',
-                leaderboard = '';
-            if (currentGame.data.players.length === 0) {
-                response.tell('Nobody has joined the game.');
-                return;
-            }
-            currentGame.data.players.forEach(function (player) {
-                sortedPlayerScores.push({
-                    score: currentGame.data.scores[player],
-                    player: player
+    intentHandlers.AddMemberIntent = function (intent, session, response) {
+        var memberName = intent.slots.Member.value;
+        if (!memberName) {
+            response.ask("Sorry, I didn\'t get that. Who would you like to add?");
+        } else {
+            storage.loadMessages(session, function (currentMessages) {
+                currentMessages.addMember(memberName, function (success) {
+                    if (success) {
+                        response.tell(memberName + " added successfully to your echo family.");
+                    } else {
+                        response.tell(memberName + " is already a part of your echo family.")
+                    }
+                    
                 });
             });
-            sortedPlayerScores.sort(function (p1, p2) {
-                return p2.score - p1.score;
-            });
-            sortedPlayerScores.forEach(function (playerScore, index) {
-                if (index === 0) {
-                    speechOutput += playerScore.player + ' has ' + playerScore.score + 'point';
-                    if (playerScore.score > 1) {
-                        speechOutput += 's';
-                    }
-                } else if (index === sortedPlayerScores.length - 1) {
-                    speechOutput += 'And ' + playerScore.player + ' has ' + playerScore.score;
+        }
+    }
+
+    intentHandlers.SendMessageIntent = function (intent, session, response) {
+        var recipient = intent.slots.Sender.value;
+        if (!recipient && !session.repeatRecipient) {
+            response.ask("Sorry, I didn\'t get that. Who would you like to deliver this message to?");
+            session.repeatRecipient = true;
+            return;
+        } else if (!recipient && session.repeatRecipient) {
+            session.repeatRecipient = false;
+            response.tell("Please, repeat your message and whom you want to send it.");
+            return;
+        }
+        else if (session.repeatRecipient && session.message) {
+            session.repeatRecipient = false;
+            session.recipient = recipient;
+            storage.saveMessage(session, function (success) {
+                if(success) {
+                    response.tell("Don't you worry! " + recipient + " will receive your message!");
+                    session.recipient = null;
+                    session.message = null;
                 } else {
-                    speechOutput += playerScore.player + ', ' + playerScore.score;
+                    response.tell("Please, repeat your message and whom you want to send it.");
                 }
-                speechOutput += '. ';
-                leaderboard += 'No.' + (index + 1) + ' - ' + playerScore.player + ' : ' + playerScore.score + '\n';
+            })
+            return;
+        }
+
+        var message = intent.slots.Message.value;
+        if (!message && !session.repeatMessage) {
+            response.ask("Sorry, I didn\'t get that. Can you repeat your message?");
+            session.repeatMessage = true;
+            return;
+        } else if (!message && session.repeatMessage) {
+            response.tell("Please, repeat your message and whom you want to send it.");
+            session.repeatMessage = false;
+            return;
+        }
+        else {
+            session.message = message;
+            session.recipient = recipient;
+            storage.saveMessage(session, function (success) {
+                if(success) {
+                    response.tell("Don't you worry! " + recipient + " will receive your message!");
+                    session.recipient = null;
+                    session.message = null;
+                } else {
+                    response.tell("I can't find " + recipient + " in your echo family. Who is that again?");
+                    session.repeatRecipient = true;
+                }
             });
-            response.tellWithCard(speechOutput, "Leaderboard", leaderboard);
-        });
-    };
-
-    intentHandlers.ResetPlayersIntent = function (intent, session, response) {
-        //remove all players
-        storage.newGame(session).save(function () {
-            response.ask('New game started without players, who do you want to add first?');
-        });
-    };
-
-    intentHandlers.HelpIntent = function (intent, session, response) {
-        var speechOutput = textHelper.completeHelp;
-        if (skillContext.needMoreHelp) {
-            response.ask(textHelper.completeHelp + ' So, how can I help?');
-        } else {
-            response.tell(textHelper.completeHelp);
+            return;
         }
-    };
-
-    intentHandlers.ExitIntent = function (intent, session, response) {
-        if (skillContext.needMoreHelp) {
-            response.tell('Okay.  Whenever you\'re ready, you can start giving points to the players in your game.');
-        } else {
-            response.tell('');
-        }
-    };
+    }
 };
 exports.register = registerIntentHandlers;
